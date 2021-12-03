24fd5157318a8cafa5e9ea9f41c80c58450d15e7
Justin Zhang <schnell18@gmail.com>
2021-12-03 11:30:55 +0800

    Bump up version 0.1.1

--- alpine/17/combine.sh
+++ alpine/17/combine.sh
@@ -1,11 +1,11 @@
-docker manifest create schnell18/zulu-java:alpine-jre17-0.1.0 \
-    --amend schnell18/zulu-java:alpine-jre17-arm64-0.1.0 \
-    --amend schnell18/zulu-java:alpine-jre17-amd64-0.1.0
+docker manifest create schnell18/zulu-java:alpine-jre17-0.1.1 \
+    --amend schnell18/zulu-java:alpine-jre17-arm64-0.1.1 \
+    --amend schnell18/zulu-java:alpine-jre17-amd64-0.1.1
 
-docker manifest push schnell18/zulu-java:alpine-jre17-0.1.0
+docker manifest push schnell18/zulu-java:alpine-jre17-0.1.1
 
-docker manifest create schnell18/zulu-java:alpine-jdk17-0.1.0 \
-    --amend schnell18/zulu-java:alpine-jdk17-arm64-0.1.0 \
-    --amend schnell18/zulu-java:alpine-jdk17-amd64-0.1.0
+docker manifest create schnell18/zulu-java:alpine-jdk17-0.1.1 \
+    --amend schnell18/zulu-java:alpine-jdk17-arm64-0.1.1 \
+    --amend schnell18/zulu-java:alpine-jdk17-amd64-0.1.1
 
-docker manifest push schnell18/zulu-java:alpine-jdk17-0.1.0
+docker manifest push schnell18/zulu-java:alpine-jdk17-0.1.1
